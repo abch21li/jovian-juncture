f1f976b9345b489ce784c940b236034c65ccdbee
Chandler Abel <abch21li@student.ju.se>
2022-10-20 10:00:53 +0200

    adjusted port

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const adminPassword =
 const db = new sqlite3.Database("jovian-database.db");
 const MIN_REVIEW_NAME_LENGTH = 2;
 const app = express();
-// const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3030;
 
 /* ----------------------------------------------------APP ENGINE*/
 /* -------------------------------------------------*/
@@ -628,8 +628,8 @@ app.get("/faq/delete/:id", function (request, response) {
 
 /* ---------------------------------------------------------------------- */
 
-// app.listen(PORT, () => {
-//   console.log(`server started on port ${PORT}`);
-// });
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
 
-app.listen(8080);
+// app.listen(8080);
